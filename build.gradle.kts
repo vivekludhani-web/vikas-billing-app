c2d083ffa77078b032b5ca1d6fe1f1be7be96576
vivekludhani-web <vivekludhani@gmail.com>
2026-09-25 11:48:21 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    kotlin("android") version "1.9.10" apply false
-    kotlin("kapt") version "1.9.10" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
+    kotlin("android") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
