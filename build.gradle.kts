40b772f4eda76c3ef2b0f4767098200fc48dd4d1
vivekludhani-web <vivekludhani@gmail.com>
2026-09-25 12:08:04 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
-    kotlin("android") version "1.9.20" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    id("com.android.application") version "8.1.0" apply false
+    kotlin("android") version "1.9.10" apply false
 }
