dfb598d5da3935b1356530a5d0efa62a53b724bd
vivekludhani-web <vivekludhani@gmail.com>
2026-09-25 21:31:56 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     kotlin("android") version "1.9.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
